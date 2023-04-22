2bea3792c504708aff3ed9fd6ff3feb8fdf99537
ajaggoe <a.s.h.jaggoe@student.tudelft.nl>
2023-04-22 17:27:21 +0200

    Jdouble?

--- src/Jq/JParser.hs
+++ src/Jq/JParser.hs
@@ -24,12 +24,13 @@ parseJNum = do JNum <$> int
 parseJNumDouble :: Parser JSON
 parseJNumDouble = do parseJDouble <|> parseJNumE
 
-parseJDouble :: Parser JSON
+parseJDouble :: Parser JSON 
 parseJDouble = do
-    full <- int
-    _ <- char '.'
-    decimal <- many digit <|> pure "0"
-    return $ JFloat (read (show full ++ "." ++ decimal))
+    l <- int 
+    _ <- symbol "."
+    r <- some digit
+    return (JFloat (read (show l ++ "." ++ r)))
+-- >>> 
 
 parseJNumE :: Parser JSON
 parseJNumE = do
