21912b33bdddd6e6fc0a84bf18f37aaf674fffb3
ajaggoe <a.s.h.jaggoe@student.tudelft.nl>
2023-06-02 11:54:16 +0200

    andiaidn

--- src/Jq/Compiler.hs
+++ src/Jq/Compiler.hs
@@ -2,6 +2,7 @@ module Jq.Compiler where
 
 import           Jq.Filters
 import           Jq.Json
+import Data.Ix (Ix(index))
 
 type JProgram a = JSON -> Either String a
 
@@ -10,7 +11,7 @@ compile (Identity) inp = return [inp]
 
 compile (Parenthesis f) inp = compile f inp
 
-compile (Indexing field) (JObject elements) = Right [lookUp field elements]
+compile (Indexing field) (JObject elements) = Right [findElem field elements]
 compile (Indexing _) JNull = Right [JNull]
 compile (Indexing _) inp = Left ("Input is not JObject: " ++ show inp)
 
@@ -19,6 +20,17 @@ compile (ArrayIndexing 0) (JArray []) = Left "Index out of bounds"
 compile (ArrayIndexing i) (JArray (x:xs)) = compile (ArrayIndexing (i-1)) (JArray xs)
 compile (ArrayIndexing _) (JNull) = Right [JNull]
 
+compile (ArraySlicer _ _) (JArray []) = Right [JArray []]
+compile (ArraySlicer start end) (JArray xs) 
+  | start < 0 = compile (ArraySlicer (length xs - min (-start) (length xs)) end) (JArray xs)
+  | end < 0 = compile (ArraySlicer start (length xs - min (-end) (length xs))) (JArray xs)
+  | start >= end = Right [JArray []]
+  | otherwise = Right [JArray (take (start - end) (drop start xs))]
+
+compile (ArrayIterator _) (JArray []) = Right [JArray []]
+compile (ArrayIterator []) (JArray _) = Right [JArray []]
+compile (ArrayIterator ind) (JArray xs) = Right [JArray (map (findByIndex xs) ind)]
+
 -- compile (ArraySlicer first second) (JArray xs) 
 --     | first >= second || xs == [] = Right [JArray []]
 --     | otherwise = [JArray ]
@@ -26,6 +38,11 @@ compile (ArrayIndexing _) (JNull) = Right [JNull]
 run :: JProgram [JSON] -> JSON -> Either String [JSON]
 run p j = p j
 
-lookUp :: String -> [(String, JSON)] -> JSON
-lookUp _ [] = JNull
-lookUp ind (x:xs) = if show ind == show (fst x) then snd x else lookUp ind xs
+findElem :: String -> [(String, JSON)] -> JSON
+findElem _ [] = JNull
+findElem ind (x:xs) = if show ind == show (fst x) then snd x else findElem ind xs
+
+findByIndex :: [JSON] -> Int -> JSON
+findByIndex xs ind
+    | ind < 0 = findByIndex xs (ind + length xs)
+    | otherwise = if ind >=0 && ind < length xs then xs!!ind else JNull
